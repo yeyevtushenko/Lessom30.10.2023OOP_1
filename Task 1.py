6fdc6ec57b2c68e30ce0f9572fe417a77cbac33a
yeyevtushenko <yehor803@gmail.com>
2023-11-07 11:01:45 +0200

    Умова завдання

--- Task 1.py
+++ Task 1.py
@@ -3,11 +3,4 @@
 # Реалізуйте методи класу для введення-виведення даних та інших операцій.
 print("Завдання №1")
 
-class Person:
-    def __init__(self, full_name, date_of_birth, phone_number, city, country, adress )
-        self.full_name = full_name
-        self.date_of_birth = date_of_birth
-        self.phone_number = phone_number
-        self.city = city
-        self.country = country
-        self.adress = adress
+
