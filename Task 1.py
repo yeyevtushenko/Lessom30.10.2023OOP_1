cf19d81ef4eb07639a7856901304078c02f1b1f9
yeyevtushenko <yehor803@gmail.com>
2023-11-07 11:00:32 +0200

    Створення класу Особа

--- Task 1.py
+++ Task 1.py
@@ -1,4 +1,13 @@
 # Реалізуйте клас «Людина». Збережіть у класі:
 # ПІБ, дату народження, контактний телефон, місто, країну, домашню адресу.
 # Реалізуйте методи класу для введення-виведення даних та інших операцій.
+print("Завдання №1")
 
+class Person:
+    def __init__(self, full_name, date_of_birth, phone_number, city, country, adress )
+        self.full_name = full_name
+        self.date_of_birth = date_of_birth
+        self.phone_number = phone_number
+        self.city = city
+        self.country = country
+        self.adress = adress
